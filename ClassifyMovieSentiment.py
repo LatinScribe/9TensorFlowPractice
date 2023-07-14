81a8f09db4a31380b3ce84c6ab6848b6b92f5c25
Henry TJ Chen <127168121+LatinScribe@users.noreply.github.com>
2023-07-14 13:04:32 -0400

    Complete dataset processing functionality
    - Removes unnecessary folders if present
    - Splits training dataset using 80:20 to create validation set
    - Tokenizes, standardizes, and vectorizes data in preparation for training

--- ClassifyMovieSentiment.py
+++ ClassifyMovieSentiment.py
@@ -18,8 +18,8 @@ import tensorflow as tf
 import random
 from DatasetDownloader import get_dataset
 
-from tensorflow.keras import layers
-from tensorflow.keras import losses
+from keras import layers
+from keras import losses
 
 URL = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
 
@@ -31,9 +31,10 @@ print('TensorFlow version:', tf.__version__)
 # DATASET_PATH, TRAIN_PATH = get_dataset(URL)
 
 if os.path.isdir('aclImdb') and os.path.isdir('aclImdb/train') and os.path.isdir('aclImdb/test'):
-    print("Dataset detected as downloaded, if issues persist, please manually redownload")
+    print('Dataset detected as downloaded, if issues persist, please manually redownload')
     DATASET_PATH = 'aclImdb'
-    TRAIN_PATH = 'aclImdb/test'
+    TRAIN_PATH = 'aclImdb/train'
+    print('Dataset downloaded sucessfully')
 else:
     print('No dataset detected')
     print('Downloading dataset, this may take a while, please wait...')
@@ -56,3 +57,109 @@ while not os.path.isfile(sample_file):
 # open and print the chosen file
 with open(sample_file) as f:
     print(f.read())
+
+# remove unnesscary folders
+remove_dir = os.path.join(TRAIN_PATH, 'unsup')
+if os.path.isdir(remove_dir):
+    print('Unesscary dataset folders detected, removing folders...')
+    shutil.rmtree(remove_dir)
+    print('Removal sucessfully completed')
+
+# Need to create a validation dataset, create one using 80:20 split
+batch_size = 32
+seed = 42
+
+raw_train_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='training',
+    seed=seed
+)
+
+# see how the training dataset is now structured
+print("Label 0 corresponds to", raw_train_ds.class_names[0])
+print("Label 1 corresponds to", raw_train_ds.class_names[1])
+
+for text_batch, label_batch in raw_train_ds.take(1):
+    for i in range(3):
+        print("Review", text_batch.numpy()[i])
+        print("Label", label_batch.numpy()[i])
+
+raw_val_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/train',
+    batch_size=batch_size,
+    validation_split=0.2,
+    subset='validation',
+    seed=seed
+)
+
+raw_test_ds = tf.keras.utils.text_dataset_from_directory(
+    'aclImdb/test',
+    batch_size=batch_size)
+
+
+# created a custom standardiser that removes HTML as well
+def custom_standardization(input_data):
+    """Standize the inputted data by converting all text to lower case
+    Also tokenizes the text and strips HTML tags
+    """
+    lowercase = tf.strings.lower(input_data)
+    stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+    return tf.strings.regex_replace(stripped_html,
+                                    '[%s]' % re.escape(string.punctuation),
+                                    '')
+
+
+max_features = 10000
+sequence_length = 250
+
+vectorize_layer = layers.TextVectorization(
+    standardize=custom_standardization,
+    max_tokens=max_features,
+    output_mode='int',
+    output_sequence_length=sequence_length
+)
+
+
+def seperate_first(x, _):
+    """takes in x and y, returns only x"""
+    return x
+
+
+# makes a text-only dataset (without labels), then call adapt
+train_text = raw_train_ds.map(seperate_first)
+vectorize_layer.adapt(train_text)
+
+
+# function to see how vectorization of layer works
+def vectorize_text(text, label):
+    """Returns vectorized text"""
+    text = tf.expand_dims(text, -1)
+    return vectorize_layer(text), label
+
+
+# retrieve a batch (of 32 reviews and labels) from the dataset
+text_batch, label_batch = next(iter(raw_train_ds))
+first_review, first_label = text_batch[0], label_batch[0]
+print("Review", first_review)
+print("Label", raw_train_ds.class_names[first_label])
+print("Vectorized review", vectorize_text(first_review, first_label))
+
+
+# check what each token corresponds to
+print("1287 ---> ", vectorize_layer.get_vocabulary()[1287])
+print(" 313 ---> ", vectorize_layer.get_vocabulary()[313])
+print('Vocabulary size: {}'.format(len(vectorize_layer.get_vocabulary())))
+
+# apply the TextVectorization layer on all components of dataset
+train_ds = raw_train_ds.map(vectorize_text)
+val_ds = raw_val_ds.map(vectorize_text)
+test_ds = raw_test_ds.map(vectorize_text)
+
+# configuring dataset for best performance
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.cache().prefetch(buffer_size=AUTOTUNE)
